a902750275fe1a1b0228cd4cf5183589870247e2
ValentinVielma <va.vielma@alumnos.duoc.cl>
2019-11-30 12:09:59 -0300

    commit final de lechitas

--- Leches/urls.py
+++ Leches/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from . import views
-from .views import update,delete
+from .views import update, delete
+from django.contrib.auth.decorators import login_required
+
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('crud', views.crud, name='crud'),
+    path('login', views.login_view, name='login'),
+    path('crud', login_required(views.crud, login_url='login'), name='crud'),
     path('list', views.list, name='list'),
     path('new', views.new, name='new'),
     path('update/<int:id>/', update, name='update'),
     path('delete/<int:id>/', delete, name='delete'),
     path('carrusel', views.carrusel, name='carrusel'),
+    path('logout', views.logout_view, name='logout'),
 ]
--- Leches/views.py
+++ Leches/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Leche
 from .forms import LecheForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
 
 
 # Create your views here.
@@ -48,5 +50,24 @@ def delete(request, id):
 
     return render(request, 'delete_leche_confirm.html', {'leche': leche})
 
+
 def carrusel(request):
     return render(request, 'carrusel.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+    return redirect('index')
